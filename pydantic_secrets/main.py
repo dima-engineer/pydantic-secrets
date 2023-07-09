3b0801fadf41d01cb3375269824d40cd98eeb026
Dmitry Marchuk <marchuk.d13@gmail.com>
2023-07-09 17:58:44 +0200

    Syntax changes

--- pydantic_secrets/main.py
+++ pydantic_secrets/main.py
@@ -1,15 +1,13 @@
 import os
 import typing as t
 import warnings
-from typing import Any
 
 from pydantic import BaseModel
 from pydantic.fields import FieldInfo
 from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
 
 from pydantic_secrets.protocols import SecretManagerClientProtocol
-
-from .warnings import SecretReceivingWarning, SecretVersionNotSpecifiedWarning
+from pydantic_secrets.warnings import SecretReceivingWarning, SecretVersionNotSpecifiedWarning
 
 EXTRA_SECRET_NAME_KEY = "secret_name"
 ENV_SECRET_NAME_KEY = "secret_name_env"
@@ -25,15 +23,13 @@ class SecretManagerSource(PydanticBaseSettingsSource):
         default_secret_version: str | None = None,
     ):
         super().__init__(settings_cls)
-        self.__secret_manager__ = secret_manager_client
-        self.__default_secret_version__ = default_secret_version
+        self.__secret_manager = secret_manager_client
+        self.__default_secret_version = default_secret_version
 
-    def get_field_value(
-        self, field: FieldInfo, field_name: str
-    ) -> tuple[Any, str, bool]:
+    def get_field_value(self, field: FieldInfo, field_name: str) -> tuple[str, t.Any, bool]:
         return field_name, field.default, self.field_is_complex(field)
 
-    def __call__(self) -> dict[str, Any]:
+    def __call__(self) -> dict[str, t.Any]:
         return self._process_fields(self.settings_cls.model_fields)
 
     def _get_secret_info_from_extra(
@@ -48,26 +44,24 @@ class SecretManagerSource(PydanticBaseSettingsSource):
         secret_version_env = extra.get(ENV_SECRET_VERSION_KEY)
         if secret_version_env:
             secret_version = os.getenv(secret_version_env) or secret_version
-        secret_version = secret_version or self.__default_secret_version__
+        secret_version = secret_version or self.__default_secret_version
         return secret_name, secret_version
 
-    def _process_fields(self, fields: dict[str, FieldInfo]):
-        d: dict[str, t.Any] = {}
+    def _process_fields(self, fields: dict[str, FieldInfo]) -> dict[str, t.Any]:
+        data: dict[str, t.Any] = {}
         for field_name, field in fields.items():
             if self.field_is_complex(field):
-                if field.annotation and issubclass(field.annotation, BaseModel):
-                    d[field_name] = self._process_fields(field.annotation.model_fields)
+                if isinstance(field.annotation, type) and issubclass(field.annotation, BaseModel):
+                    data[field_name] = self._process_fields(field.annotation.model_fields)
                     continue
-                d[field_name] = field
+                data[field_name] = field
                 continue
             secret_name, secret_version = (
-                self._get_secret_info_from_extra(field.json_schema_extra)
-                if field.json_schema_extra
-                else (None, None)
+                self._get_secret_info_from_extra(field.json_schema_extra) if field.json_schema_extra else (None, None)
             )
             if secret_name and secret_version:
                 try:
-                    d[field_name] = self.__secret_manager__.get_secret(
+                    data[field_name] = self.__secret_manager.get_secret(
                         secret_name=secret_name,
                         secret_version=secret_version,
                     )
@@ -76,4 +70,4 @@ class SecretManagerSource(PydanticBaseSettingsSource):
                     pass
             elif secret_name and not secret_version:
                 warnings.warn(field_name, category=SecretVersionNotSpecifiedWarning)
-        return d
+        return data
