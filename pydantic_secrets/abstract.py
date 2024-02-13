542ea9261581de7773e8490f14320ff58ef1198b
Dmitry Marchuk <marchuk.d13@gmail.com>
2024-02-13 23:25:42 +0100

    Add docstrings for abstract class and protocol

--- pydantic_secrets/abstract.py
+++ pydantic_secrets/abstract.py
@@ -4,4 +4,9 @@ from abc import ABC, abstractmethod
 class SecretManagerClientABC(ABC):
     @abstractmethod
     def get_secret(self, secret_name: str, secret_version: str) -> str:
-        ...
+        """
+        Returns a secret from secret manager by secret name and secret version
+        :param secret_name: Name of secret to be fetched
+        :param secret_version: Version of the secret to be fetched
+        :return: Secret represented as a string
+        """
--- pydantic_secrets/protocols.py
+++ pydantic_secrets/protocols.py
@@ -3,4 +3,9 @@ from typing import Protocol
 
 class SecretManagerClientProtocol(Protocol):
     def get_secret(self, secret_name: str, secret_version: str) -> str:
-        ...
+        """
+        Returns a secret from secret manager by secret name and secret version
+        :param secret_name: Name of secret to be fetched
+        :param secret_version: Version of the secret to be fetched
+        :return: Secret represented as a string
+        """
